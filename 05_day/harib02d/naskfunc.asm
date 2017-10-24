c59a98d856b455e4569e056ec89c1b2577b5f00d
CHOE DONG HOON <sapsaldog@gmail.com>
2017-10-24 16:37:24 +0900

    day 05 - harib02d revision

---
+++ 05_day/harib02d/naskfunc.asm
@@ -0,0 +1,76 @@
+; naskfunc
+; TAB=4
+
+[bits 32]					; 32비트 모드용의 기계어를 만든다
+
+GLOBAL	io_hlt, io_cli, io_sti, io_stihlt
+GLOBAL	io_in8, io_in16,  io_in32
+GLOBAL	io_out8, io_out16, io_out32
+GLOBAL	io_load_eflags, io_store_eflags
+
+_start:
+    [extern HariMain] ; Define calling point. Must have same name as kernel.c 'main' function
+    call HariMain ; Calls the C function. The linker will know where it is placed in memory
+
+io_hlt:	; void io_hlt(void);
+		HLT
+		RET
+
+io_cli:	; void io_cli(void);
+		CLI
+		RET
+
+io_sti:	; void io_sti(void);
+		STI
+		RET
+
+io_stihlt:	; void io_stihlt(void);
+		STI
+		HLT
+		RET
+
+io_in8:	; int io_in8(int port);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,0
+		IN		AL,DX
+		RET
+
+io_in16:	; int io_in16(int port);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,0
+		IN		AX,DX
+		RET
+
+io_in32:	; int io_in32(int port);
+		MOV		EDX,[ESP+4]		; port
+		IN		EAX,DX
+		RET
+
+io_out8:	; void io_out8(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		AL,[ESP+8]		; data
+		OUT		DX,AL
+		RET
+
+io_out16:	; void io_out16(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,[ESP+8]		; data
+		OUT		DX,AX
+		RET
+
+io_out32:	; void io_out32(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,[ESP+8]		; data
+		OUT		DX,EAX
+		RET
+
+io_load_eflags:	; int io_load_eflags(void);
+		PUSHFD		; PUSH EFLAGS의 의미
+		POP		EAX
+		RET
+
+io_store_eflags:	; void io_store_eflags(int eflags);
+		MOV		EAX,[ESP+4]
+		PUSH	EAX
+		POPFD		; POP EFLAGS의 의미
+		RET
