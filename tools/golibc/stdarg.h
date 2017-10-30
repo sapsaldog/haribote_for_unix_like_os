a4a0c098cc0e65d0d823c0c878027843f69a1f44
CHOE DONG HOON <sapsaldog@gmail.com>
2017-10-30 17:11:37 +0900

    stdarg header bugfix

--- tools/golibc/stdarg.h
+++ tools/golibc/stdarg.h
@@ -8,7 +8,7 @@
 	extern "C" {
 #endif
 
-#define va_start(v,l)	__builtin_va_start
+#define va_start(v,l)	__builtin_va_start((v),l)
 #define va_end			__builtin_va_end
 #define va_arg			__builtin_va_arg
 #define va_copy(d,s)	__builtin_va_copy((d),(s))
