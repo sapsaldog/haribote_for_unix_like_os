da877f10fd258becfd8824f19a8abae6dcf0726b
CHOE DONG HOON <sapsaldog@gmail.com>
2017-10-31 14:55:28 +0900

    revision 06_day/harib03e

---
+++ 06_day/harib03e/naskfunc.asm
@@ -0,0 +1,121 @@
+; naskfunc
+; TAB=4
+
+[bits 32]					; 32비트 모드용의 기계어를 만든다 
+
+GLOBAL	io_hlt, io_cli, io_sti, io_stihlt
+GLOBAL	io_in8, io_in16,  io_in32
+GLOBAL	io_out8, io_out16, io_out32
+GLOBAL	io_load_eflags, io_store_eflags
+GLOBAL	load_gdtr, load_idtr
+GLOBAL	asm_inthandler21, asm_inthandler2c
+EXTERN	inthandler21, inthandler2c
+
+[SECTION .text]
+
+io_hlt:	; void io_hlt(void);
+		HLT
+		RET
+
+io_cli:	; void io_cli(void);
+		CLI
+		RET
+
+io_sti:	; void io_sti(void);
+		STI
+		RET
+
+io_stihlt:	; void io_stihlt(void);
+		STI
+		HLT
+		RET
+
+io_in8:	; int io_in8(int port);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,0
+		IN		AL,DX
+		RET
+
+io_in16:	; int io_in16(int port);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,0
+		IN		AX,DX
+		RET
+
+io_in32:	; int io_in32(int port);
+		MOV		EDX,[ESP+4]		; port
+		IN		EAX,DX
+		RET
+
+io_out8:	; void io_out8(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		AL,[ESP+8]		; data
+		OUT		DX,AL
+		RET
+
+io_out16:	; void io_out16(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,[ESP+8]		; data
+		OUT		DX,AX
+		RET
+
+io_out32:	; void io_out32(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,[ESP+8]		; data
+		OUT		DX,EAX
+		RET
+
+io_load_eflags:	; int io_load_eflags(void);
+		PUSHFD		; PUSH EFLAGS의 의미
+		POP		EAX
+		RET
+
+io_store_eflags:	; void io_store_eflags(int eflags);
+		MOV		EAX,[ESP+4]
+		PUSH	EAX
+		POPFD		; POP EFLAGS 라고 하는 의미
+		RET
+
+load_gdtr:		; void load_gdtr(int limit, int addr);
+		MOV		AX,[ESP+4]		; limit
+		MOV		[ESP+6],AX
+		LGDT	[ESP+6]
+		RET
+
+load_idtr:		; void load_idtr(int limit, int addr);
+		MOV		AX,[ESP+4]		; limit
+		MOV		[ESP+6],AX
+		LIDT	[ESP+6]
+		RET
+
+asm_inthandler21:
+		PUSH	ES
+		PUSH	DS
+		PUSHAD
+		MOV		EAX,ESP
+		PUSH	EAX
+		MOV		AX,SS
+		MOV		DS,AX
+		MOV		ES,AX
+		CALL	inthandler21
+		POP		EAX
+		POPAD
+		POP		DS
+		POP		ES
+		IRETD
+
+asm_inthandler2c:
+		PUSH	ES
+		PUSH	DS
+		PUSHAD
+		MOV		EAX,ESP
+		PUSH	EAX
+		MOV		AX,SS
+		MOV		DS,AX
+		MOV		ES,AX
+		CALL	inthandler2c
+		POP		EAX
+		POPAD
+		POP		DS
+		POP		ES
+		IRETD
