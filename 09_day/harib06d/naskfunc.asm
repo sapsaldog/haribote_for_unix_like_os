0d4391625409f2d0f8e711e76db2994c09f24999
CHOE DONG HOON <sapsaldog@gmail.com>
2017-10-31 15:54:42 +0900

    revision 09_day/harib06d

--- 09_day/harib06d/naskfunc.asm
+++ 09_day/harib06d/naskfunc.asm
@@ -1,109 +1,105 @@
 ; naskfunc
 ; TAB=4
 
-[FORMAT "WCOFF"]				; 오브젝트 파일을 만드는 모드	
-[INSTRSET "i486p"]				; 486명령까지 사용하고 싶다고 하는 기술
-[BITS 32]					; 32비트 모드용의 기계어를 만들게 한다
-[FILE "naskfunc.nas"]				; 원시 파일명 정보
+[bits 32]					; 32비트 모드용의 기계어를 만든다 
 
-		GLOBAL	_io_hlt, _io_cli, _io_sti, _io_stihlt
-		GLOBAL	_io_in8,  _io_in16,  _io_in32
-		GLOBAL	_io_out8, _io_out16, _io_out32
-		GLOBAL	_io_load_eflags, _io_store_eflags
-		GLOBAL	_load_gdtr, _load_idtr
-		GLOBAL	_load_cr0, _store_cr0
-		GLOBAL	_asm_inthandler21, _asm_inthandler2c
-		GLOBAL	_memtest_sub
-		EXTERN	_inthandler21, _inthandler2c
-
+GLOBAL	io_hlt, io_cli, io_sti, io_stihlt
+GLOBAL	io_in8, io_in16,  io_in32
+GLOBAL	io_out8, io_out16, io_out32
+GLOBAL	io_load_eflags, io_store_eflags
+GLOBAL	load_gdtr, load_idtr
+GLOBAL	load_cr0, store_cr0
+GLOBAL	asm_inthandler21, asm_inthandler2c
+GLOBAL	memtest_sub
+EXTERN	inthandler21, inthandler2c
 
 [SECTION .text]
 
-_io_hlt:	; void io_hlt(void);
+io_hlt:	; void io_hlt(void);
 		HLT
 		RET
 
-_io_cli:	; void io_cli(void);
+io_cli:	; void io_cli(void);
 		CLI
 		RET
 
-_io_sti:	; void io_sti(void);
+io_sti:	; void io_sti(void);
 		STI
 		RET
 
-_io_stihlt:	; void io_stihlt(void);
+io_stihlt:	; void io_stihlt(void);
 		STI
 		HLT
 		RET
 
-_io_in8:	; int io_in8(int port);
+io_in8:	; int io_in8(int port);
 		MOV		EDX,[ESP+4]		; port
 		MOV		EAX,0
 		IN		AL,DX
 		RET
 
-_io_in16:	; int io_in16(int port);
+io_in16:	; int io_in16(int port);
 		MOV		EDX,[ESP+4]		; port
 		MOV		EAX,0
 		IN		AX,DX
 		RET
 
-_io_in32:	; int io_in32(int port);
+io_in32:	; int io_in32(int port);
 		MOV		EDX,[ESP+4]		; port
 		IN		EAX,DX
 		RET
 
-_io_out8:	; void io_out8(int port, int data);
+io_out8:	; void io_out8(int port, int data);
 		MOV		EDX,[ESP+4]		; port
 		MOV		AL,[ESP+8]		; data
 		OUT		DX,AL
 		RET
 
-_io_out16:	; void io_out16(int port, int data);
+io_out16:	; void io_out16(int port, int data);
 		MOV		EDX,[ESP+4]		; port
 		MOV		EAX,[ESP+8]		; data
 		OUT		DX,AX
 		RET
 
-_io_out32:	; void io_out32(int port, int data);
+io_out32:	; void io_out32(int port, int data);
 		MOV		EDX,[ESP+4]		; port
 		MOV		EAX,[ESP+8]		; data
 		OUT		DX,EAX
 		RET
 
-_io_load_eflags:	; int io_load_eflags(void);
-		PUSHFD		; PUSH EFLAGS 라고 하는 의미
+io_load_eflags:	; int io_load_eflags(void);
+		PUSHFD		; PUSH EFLAGS의 의미
 		POP		EAX
 		RET
 
-_io_store_eflags:	; void io_store_eflags(int eflags);
+io_store_eflags:	; void io_store_eflags(int eflags);
 		MOV		EAX,[ESP+4]
 		PUSH	EAX
 		POPFD		; POP EFLAGS 라고 하는 의미
 		RET
 
-_load_gdtr:		; void load_gdtr(int limit, int addr);
+load_gdtr:		; void load_gdtr(int limit, int addr);
 		MOV		AX,[ESP+4]		; limit
 		MOV		[ESP+6],AX
 		LGDT	[ESP+6]
 		RET
 
-_load_idtr:		; void load_idtr(int limit, int addr);
+load_idtr:		; void load_idtr(int limit, int addr);
 		MOV		AX,[ESP+4]		; limit
 		MOV		[ESP+6],AX
 		LIDT	[ESP+6]
 		RET
 
-_load_cr0:		; int load_cr0(void);
+load_cr0:		; int load_cr0(void);
 		MOV		EAX,CR0
 		RET
 
-_store_cr0:		; void store_cr0(int cr0);
+store_cr0:		; void store_cr0(int cr0);
 		MOV		EAX,[ESP+4]
 		MOV		CR0,EAX
 		RET
 
-_asm_inthandler21:
+asm_inthandler21:
 		PUSH	ES
 		PUSH	DS
 		PUSHAD
@@ -112,14 +108,14 @@ _asm_inthandler21:
 		MOV		AX,SS
 		MOV		DS,AX
 		MOV		ES,AX
-		CALL	_inthandler21
+		CALL	inthandler21
 		POP		EAX
 		POPAD
 		POP		DS
 		POP		ES
 		IRETD
 
-_asm_inthandler2c:
+asm_inthandler2c:
 		PUSH	ES
 		PUSH	DS
 		PUSHAD
@@ -128,14 +124,14 @@ _asm_inthandler2c:
 		MOV		AX,SS
 		MOV		DS,AX
 		MOV		ES,AX
-		CALL	_inthandler2c
+		CALL	inthandler2c
 		POP		EAX
 		POPAD
 		POP		DS
 		POP		ES
 		IRETD
 
-_memtest_sub:	; unsigned int memtest_sub(unsigned int start, unsigned int end)
+memtest_sub:	; unsigned int memtest_sub(unsigned int start, unsigned int end)
 		PUSH	EDI						; (EBX, ESI, EDI 도 사용하고 싶기 때문에)
 		PUSH	ESI
 		PUSH	EBX
